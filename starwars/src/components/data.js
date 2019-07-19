d9f5424533fdca2aa9f710fb4a20d8cb31e62867
bquizza <blaine555@gmail.com>
2019-07-19 11:35:04 -0600

    almost done

---
+++ starwars/src/components/data.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card } from './appcard'
+
+
+export const GetData = () =>{
+    const [peopleData, setPeopleData] = useState()
+    useEffect(() => {
+
+        axios
+            .get('https://swapi.co/api/people/')
+            .then(res => {
+                console.log(res.data.results)
+                setPeopleData(res.data.results)
+                
+                
+                
+
+            })
+            .catch(error => {
+                console.log('fetch error: ' + error)
+            })
+    }, [])
+
+    if (!peopleData) {
+        return <h1>loading</h1>
+    } else {
+
+    return (
+        <div>
+            {
+                peopleData.map(person => {
+                    return (
+                    
+                    
+                    <Card name={person.name} height={person.height} mass={person.mass} hair_color={person.hair_color} skin_color={person.skin_color} gender={person.gender} birth_year={person.birth_year} />
+                    
+                    )
+                   })
+            }
+
+
+        </div>
+    )
+    
+}
+}
